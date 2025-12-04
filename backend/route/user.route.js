d9e1cc1e708fd8b922602c1cd47bce6c22f181c5
Jeet <jeetmaheshwari1919@gmail.com>
2025-12-04 18:46:30 +0530

    feat: add channel creation functionality with avatar and banner uploads

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getCurrentUser } from "../controller/user.controller.js";
+import { createChannel, getCurrentUser } from "../controller/user.controller.js";
 import { verifyUser } from "../middleware/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/get-user", verifyUser, getCurrentUser);
+router.post("/createchannel", verifyUser, uploadOnCloudinary.fields[
+    { name: "avatar", maxCount:1 },
+    { name: "banner", maxCount:1 }
+], createChannel);
 
 export default router;
